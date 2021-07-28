ab7588602526da486803ce4aaf03a6442659ed4f
Ranjith054 <ranjith.awful@gmail.com>
2021-07-28 23:53:48 +0530

    commit

--- djangochat/urls.py
+++ djangochat/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from django.conf import settings,
+from django.conf import settings
 from django.views.static import serve
 
 
