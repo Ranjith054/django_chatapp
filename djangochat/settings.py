ad4fe4c23e960d9bb186a16bc6df2fc91d0bfd75
Ranjith054 <ranjith.awful@gmail.com>
2021-07-28 23:04:22 +0530

    commit

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,5 +128,3 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
